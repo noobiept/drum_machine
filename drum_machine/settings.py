4b4b0571ed26f42b54429750c639defd87b621fe
Pedro Ferreira <plferreira4@gmail.com>
2017-04-03 17:09:23 +0100

    - fix, only set the allowed hosts on live server (so we can test it in localhost) - update the middleware setting

--- drum_machine/settings.py
+++ drum_machine/settings.py
@@ -1,10 +1,4 @@
 """
-    For more information on this file, see
-    https://docs.djangoproject.com/en/1.8/topics/settings/
-
-    For the full list of settings and their values, see
-    https://docs.djangoproject.com/en/1.8/ref/settings/
-
     Needs some environment strings in the live server.
 
         LIVE -- To know its in the live server.
@@ -34,7 +28,8 @@ else:
     SECRET_KEY = os.environ[ 'SECRET_KEY' ]
 
 
-ALLOWED_HOSTS = [ 'drum-machine.heroku.com', 'drum-machine.herokuapp.com' ]
+if not DEBUG:
+    ALLOWED_HOSTS = [ 'drum-machine.heroku.com', 'drum-machine.herokuapp.com' ]
 
 
 INSTALLED_APPS = (
@@ -51,16 +46,15 @@ INSTALLED_APPS = (
 )
 
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
-)
+]
 
 
 ROOT_URLCONF = 'drum_machine.urls'
@@ -88,7 +82,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'drum_machine.wsgi.application'
 
 
-# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -97,22 +90,15 @@ DATABASES = {
 }
 
 # Internationalization
-# https://docs.djangoproject.com/en/1.8/topics/i18n/
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.8/howto/static-files/
 STATIC_URL = '/static/'
-
 STATIC_ROOT = os.path.join( BASE_DIR, 'static_root' )
 
 
