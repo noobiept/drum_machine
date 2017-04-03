22dd650d062d928f00c7bf155c1206cdbaf21567
Pedro Ferreira <plferreira4@gmail.com>
2017-04-03 17:11:54 +0100

    - use the 'whitenoise' library to serve static files

--- drum_machine/urls.py
+++ drum_machine/urls.py
@@ -22,10 +22,3 @@ urlpatterns = [
 
     url( r'^admin/', include( admin.site.urls ) ),
 ]
-
-
-    # Serve static files when debug false
-if not settings.DEBUG:
-    urlpatterns += [
-        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-    ]
